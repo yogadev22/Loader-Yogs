373aa8821e812798e74227d2f97029ee56411cd2
kembaryogayogi01-web <kembaryogayogi01@gmail.com>
2026-05-02 21:45:58 +0800

    anjayy

---
+++ server/obj/local/arm64-v8a/objs/fuck/oxorany.o.d
@@ -0,0 +1,9 @@
+./obj/local/arm64-v8a/objs/fuck/oxorany.o: jni\oxorany.cpp jni\oxorany.h \
+  D:\SDK\ndk\24.0.8215888\build\\..\sources\cxx-stl\llvm-libc++\include\stdint.h \
+  D:\SDK\ndk\24.0.8215888\build\\..\sources\cxx-stl\llvm-libc++\include\__config
+
+jni\oxorany.h:
+
+D:\SDK\ndk\24.0.8215888\build\\..\sources\cxx-stl\llvm-libc++\include\stdint.h:
+
+D:\SDK\ndk\24.0.8215888\build\\..\sources\cxx-stl\llvm-libc++\include\__config:
